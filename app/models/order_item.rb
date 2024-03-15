a70c45b84eb44a8bfdc58b750527bc2e6704c478
tacooyaki <p_mercredi@yahoo.ca>
2024-03-14 23:28:46 -0500

    Defined ransack associations for the prduct, order_item and admin_user controllers. Defined ransack attributes for the products controller.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id order_id product_id quantity unit_price created_at updated_at]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[category order_items]
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name description price category_id created_at updated_at]
+  end
 end
