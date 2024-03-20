7780cc710bf450457491aee80d91f7085ea8bb0c
tacooyaki <p_mercredi@yahoo.ca>
2024-03-19 23:06:34 -0500

    Created Active Admin models for categories and products. Whitelisted the ransackable associations and attributes in the category model.

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,38 +1,32 @@
-ActiveAdmin.register Product do
+ActiveAdmin.register Category do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description, :price, :category_id
+  # permit_params :name
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :description, :price, :category_id]
+  #   permitted = [:name]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  #
-  permit_params :name, :description, :price, :category_id
+
+  permit_params :name
 
   index do
     selectable_column
     id_column
     column :name
-    column :description
-    column :price
-    column :category
     actions
   end
 
   form do |f|
     f.inputs do
       f.input :name
-      f.input :description
-      f.input :price
-      f.input :category, as: :select, collection: Category.all
     end
     f.actions
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ApplicationRecord
   has_many :products
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[products]
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name created_at updated_at]
+  end
+
 end
