079599eaea678004efd9d5abe80dc76aca60b50d
tacooyaki <p_mercredi@yahoo.ca>
2024-03-26 15:15:47 -0500

    Added ability to add or update images for products. Added a test product category and product with a test image via rails console.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,21 +1,13 @@
 ActiveAdmin.register Product do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :price, :category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  #
-  permit_params :name, :description, :price, :category_id
+  # Alters automatic image filters causing conflicts with ActiveAdmin/Ransack
+  filter :name
+  filter :price
+  filter :category, as: :select, collection: -> { Category.all }
+
+  remove_filter :images
+
+  permit_params :name, :description, :price, :category_id, images: []
 
   index do
     selectable_column
@@ -28,12 +20,31 @@ ActiveAdmin.register Product do
   end
 
   form do |f|
-    f.inputs do
+    f.inputs 'Product Details' do
       f.input :name
       f.input :description
       f.input :price
       f.input :category, as: :select, collection: Category.all
+      f.input :images, as: :file, input_html: { multiple: true }
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :category
+      row :images do |product|
+        div do
+          product.images.each do |image| # Ignore error message
+            span do
+              image_tag url_for(image)
+            end
+          end
+        end
+      end
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many_attached :images
   belongs_to :category
   has_many :order_items
 
@@ -9,4 +10,5 @@ class Product < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     %w[id name description price category_id created_at updated_at]
   end
+
 end
